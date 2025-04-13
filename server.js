d250622c552a9400d1e1b34d36886025e5308a9d
dhiraj8408 <madhavaramdhiraj2005@gmail.com>
2025-04-13 17:00:07 +0530

    Updated frontend_URL

--- server.js
+++ server.js
@@ -29,15 +29,15 @@ let transport = nodemailer.createTransport({
 });
 const PORT = process.env.PORT || 5000;
 
-if(process.env.NODE_STATUS === "development" ) {
+if (process.env.NODE_STATUS === "development") {
   app.use(cors());
 }
 else {
   app.use(
-  cors({
-    origin: ["https://astrovnit.in", "http://localhost:3000"],
-  })
-);
+    cors({
+      origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    })
+  );
 }
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
